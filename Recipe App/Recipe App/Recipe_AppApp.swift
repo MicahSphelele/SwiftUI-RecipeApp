1d6fd38714a9d0b01806ae17df6abf746c75d7c9
Sphelele <59883549+sphelele-ikhokha@users.noreply.github.com>
2021-06-11 21:34:25 +0200

    - Initial commit 🖥

---
+++ Recipe App/Recipe App/Recipe_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_AppApp.swift
+//  Recipe App
+//
+//  Created by Sphelele Ngubane on 2021/06/11.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
