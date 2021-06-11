8bd0f5066da8688b48f7b1699e2bdc10ab059f6a
Gontze <gontse@ikhokha.com>
2021-06-11 22:00:34 +0200

    Added Recipe Model

---
+++ Recipe-App/Recipe-App/Models/Recipe.swift
@@ -0,0 +1,22 @@
+//
+//  Recipe.swift
+//  Recipe-App
+//
+//  Created by Gontse Ranoto on 2021/06/11.
+//
+
+import Foundation
+
+struct Recipe {
+    let id: Int?
+    let title:String?
+    let publisher:String?
+    let featuredImage:String?
+    let rating:Int?
+    let sourceUrl:String?
+    let description:String?
+    let cookingInstructions:String?
+    let ingredients: [String]?
+    let dateAdded:String?
+    let dateUpdated:String?
+}
