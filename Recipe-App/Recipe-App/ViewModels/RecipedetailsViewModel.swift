8f558e058d08b3e70ad8d96255a691d3142f6e85
Sphelele <sphemicah@gmail.com>
2021-06-11 23:02:14 +0200

    - Created App viewmodels 💻

---
+++ Recipe-App/Recipe-App/ViewModels/RecipedetailsViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  RecipedetailsViewModel.swift
+//  Recipe-App
+//
+//  Created by Sphelele Ngubane on 2021/06/11.
+//
+
+import SwiftUI
+
+class RecipedetailsViewModel: ObservableObject {
+    
+}
