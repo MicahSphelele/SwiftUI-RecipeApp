353a6ece71a5c896d72f7a52729262c9f17f67e6
Gontze <gontse@ikhokha.com>
2021-06-11 22:20:51 +0200

    Added Recipe Category

---
+++ Recipe-App/Recipe-App/Models/FoodCategory.swift
@@ -0,0 +1,25 @@
+//
+//  FoodCategory.swift
+//  Recipe-App
+//
+//  Created by Gontse Ranoto on 2021/06/11.
+//
+
+import Foundation
+
+enum FoodCategory: String {
+    case CHICKEN = "Chicken"
+    case BEEF = "Beef"
+    case SOUP = "Soup"
+    case DESSERT = "Dessert"
+    case VEGETARIAN = "Vegetarian"
+    case MILK = "Milk"
+    case VEGAN = "Vegan"
+    case PIZZA = "Pizza"
+    case DONUT = "Donut"
+    
+    func getFoodCategory(_ value: String) -> Optional<FoodCategory>  {
+        guard let result = FoodCategory.init(rawValue: value) else { return nil }
+        return result
+    }
+}
